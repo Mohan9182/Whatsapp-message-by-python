b0cb70feaefe2179a84fac6f9c9c966cfc8290be
Mohan9182 <42374539+Mohan9182@users.noreply.github.com>
2020-06-07 17:16:43 +0530

    Update whatapp.py

--- whatapp.py
+++ whatapp.py
@@ -1,22 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+import pyautogui as pg
 
-options = webdriver.ChromeOptions()
-options.binary_location = r"Browser_location"
-chromedriver = "chromedriver_location"
-driver = webdriver.Chrome(chromedriver,chrome_options=options)
-driver.get('https://web.whatsapp.com/')
-
-name = input('Enter Receiver name:')
-msg = input('Enter Message:')
-wait = WebDriverWait(driver,timeout=900)
-
-#For searching the user 
-user = driver.find_elements_by_xpath('//span[@title = "%s"]'%name)
-user.click()
-
-#For Enterng Message and Clicking send button
-msg_box = driver.find_element_by_class_name('_2S1VP')
-msg_box.send_keys(msg)
-button = driver.find_element_by_class_name('_35EW6')
-button.click()
+def sendwhatmsg(phone_no, message):
+  web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+message)
+  pg.press('enter')
+  
+if __name__ == "__main__":
+  phone_no = "+91"+input("Enter the Receiver Phone Number:")
+  message = input("Enter Message:")
+  sendwhatmsg(phone_no,message)
